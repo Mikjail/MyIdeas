6f252154bfd3bf00dc1f354bcc99d7f15b4ac9f0
mikjail <mikjailsalazar@gmail.com>
2016-04-06 17:38:01 -0300

    updating!

---
+++ Lesson4.CreateTXT/getIn.php
@@ -0,0 +1,15 @@
+<?php 
+		var_dump($_POST);
+
+		if (isset($_POST["signup"])) {
+			$name = $_POST["name"];
+			$lastName = $_POST["lastName"];
+			$bday = $_POST["dateBirth"];
+			$gender = $_POST["gender"];
+			$city = $_POST["city"];
+			$email = $_POST["email"];
+			$password = $_POST["password"];
+
+
+		}
+ ?>
