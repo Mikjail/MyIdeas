120030cab6b8829ed448b0d7ea8fb5ef82159dbc
mikjail <mikjailsalazar@gmail.com>
2016-04-18 17:29:48 -0300

    updating

---
+++ Lesson4.CreateTXT/tp/Administracion.php
@@ -0,0 +1,23 @@
+<?php 
+	include_once("Empleado.php");
+
+	if(isset($_POST["Nombre"]) && isset($_POST["Apellido"]) && isset($_POST["Sexo"]) && isset($_POST["Dni"]) ){
+	 echo "<a href='Form.php'>Los datos fueron enviados!</a>" ;
+
+
+	 $nombre= $_POST["Nombre"];
+	 $apellido = $_POST["Apellido"];
+	 $sexo = $_POST["Sexo"];
+	 $dni = (int)$_POST["Dni"];
+	 $legajo = (int)$_POST["Legajo"];
+	 $sueldo = (double)$_POST["Sueldo"];
+
+
+	$empleado = new Empleado($nombre, $dni, $apellido, $sexo, $legajo, $sueldo);
+	}
+
+	echo "<br>";
+
+	echo $empleado->toString();
+
+ ?>
--- Lesson4.CreateTXT/tp/Form.php
+++ Lesson4.CreateTXT/tp/Form.php
@@ -25,37 +25,49 @@
 		<hr>
 		<div class="col-md-6">
 	
-		<form data-toggle="validator" role="form" action="Administrcion.php" method="POST">
+		<form data-toggle="validator" role="form" action="Administracion.php" method="POST">
 		<div class="form-group">
 		    <label for="inputName" class="control-label">Name</label>
-		    <input type="text" class="form-control" id="inputName" pattern="^[A-z]{1,}$" maxlength="15" placeholder="First Name*" data-error="We only accept a-Z names!" required>
+		    <input type="text" name="Nombre" class="form-control" id="inputName" pattern="^[A-z]{1,}$" maxlength="15" placeholder="First Name*" data-error="We only accept a-Z names!" required>
 			<div class="help-block with-errors">A-z</div>
 		</div>
 		
 		<div class="form-group">
 		    <label for="inputName" class="control-label">Last Name</label>
-		    <input type="text" class="form-control" id="inputName" pattern="^[A-z]{1,}$" maxlength="15" placeholder="Last Name*" data-error="We only accept a-Z names!" required>
+		    <input type="text" name="Apellido" class="form-control" id="inputName" pattern="^[A-z]{1,}$" maxlength="15" placeholder="Last Name*" data-error="We only accept a-Z names!" required>
 		    <div class="help-block with-errors">A-z</div>
 		</div>
 
 		<div class="form-group">
-		    <label for="birthDate" class="control-label">Date of Birth</label>
-		    <input type="text" class="form-control" pattern="(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d" value="" name="dates_pattern2" id="dates_pattern2" list="dates_pattern2_datalist" placeholder="DD/MM/YYY" required>
-		    <div class="help-block with-errors">DD/MM/YYYY</div>
-		    </span>
+		    <label for="inputName" class="control-label">Dni</label>
+		    <input type="text" name="Dni" class="form-control" id="inputName" pattern="^[0-9]{1,}$" maxlength="15" placeholder="Dni*" data-error="We only accept numbers!" required>
+		    <div class="help-block with-errors">0-9</div>
 		</div>
+
+		<div class="form-group">
+		    <label for="inputName" class="control-label">legajo</label>
+		    <input type="text" name="Legajo" class="form-control" id="inputName" pattern="^[0-9]{1,}$" maxlength="15" placeholder="legajo*" data-error="We only accept numbers!" required>
+		    <div class="help-block with-errors">0-9</div>
+		</div>
+		
+		<div class="form-group">
+		    <label for="inputName" class="control-label">Sueldo</label>
+		    <input type="text" name="Sueldo" class="form-control" id="inputName" pattern="^[0-9]{1,}$" maxlength="15" placeholder="Sueldo*" data-error="We only accept numbers!" required>
+		    <div class="help-block with-errors">0-9</div>
+		</div>
+		
 		
 			<div class="form-group">
 			<label for="gender">Gender</label>
 		    <div class="radio">
 		      <label>
-		        <input type="radio" name="underwear" required>
+		        <input type="radio" name="Sexo" value="masculino" required>
 		        Male
 		      </label>
 		    </div>
 		    <div class="radio">
 		      <label>
-		        <input type="radio" name="underwear" required>
+		        <input type="radio" name="Sexo" value="femenino" required>
 		        Female
 		      </label>
 		    </div>
