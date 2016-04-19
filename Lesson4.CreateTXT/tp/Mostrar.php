50a73d187024c555ccadd1b20d941a340f9b6012
mikjail <mikjailsalazar@gmail.com>
2016-04-19 10:54:18 -0300

    updating mostrar

--- Lesson4.CreateTXT/tp/Mostrar.php
+++ Lesson4.CreateTXT/tp/Mostrar.php
@@ -4,6 +4,10 @@ $archivo = fopen("archivosTP3/empleados.txt", "r");
 $listaEmpleados = array();
 
 
+
+
+	echo "<table border='1 px solid black'><tr><th>Nombre</th><th>Apellido</th><th>Dni</th><th>Sexo</th><th>Legajo</th><th>Sueldo</th></tr>";
+
 while(!feof($archivo)){
 	$linea = fgets($archivo);
 	$empleado = explode("<br>", $linea);
@@ -17,13 +21,10 @@ while(!feof($archivo)){
 	 $legajo = (int)$empleado[4];
 	 $sueldo = (double)$empleado[5];
 
+	 echo "<tr><td>$nombre</td><td>$apellido</td><td>$dni</td><td>$sexo</td><td>$legajo</td><td>$sueldo</td>";
 	 	 $listaEmpleados[] = new Empleado($nombre, $dni, $apellido, $sexo, $legajo, $sueldo);
 	}
 
 }
 	fclose($archivo);
 
-	foreach ($listaEmpleados as $lista) {
-		
-		echo $lista->toString();
-	}
